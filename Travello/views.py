3580e13cfab4d833a15ad6f905ce04b018ffbc07
Akilarasan1 <akilarasan20@gmail.com>
2023-08-26 23:46:46 +0530

    sql

--- Travello/views.py
+++ Travello/views.py
@@ -6,28 +6,7 @@ from .models import Destination
 
 def index(request):
 
-    dest1 = Destination()
-    dest1.name = "Mumbai"
-    dest1.desc = 'The City that Never Sleeps'
-    dest1.img = 'destination_1.jpg'
-    dest1.price = 700
-    dest1.offer=True
-    
-    dest2 = Destination()
-    dest2.name = "Chennai"
-    dest2.desc = "Its a Wonderful City"
-    dest2.img='destination_2.jpg'
-    dest2.price = 500
-    dest2.offer=False
-    
-    dest3 = Destination()
-    dest3.name = "Bangalore"
-    dest3.desc = "City of Nights"
-    dest3.img='destination_3.jpg'
-    dest3.price = 800
-    dest2.offer =True
-    
-    dests=[dest1,dest2,dest3]
+    dests=Destination.objects.all()
     
     return render(request, 'index.html', {'dests':dests})
     
--- Trial/settings.py
+++ Trial/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "Travello.apps.TravelloConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -128,3 +129,6 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
